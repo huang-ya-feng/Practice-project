71b49e88a1c04bd9bcba73a3629408ece505ecf9
huangyf <king_7319@163.com>
2022-09-04 22:07:08 +0800

    提交

--- practice-react-h5/src/setupProxy.js
+++ practice-react-h5/src/setupProxy.js
@@ -1,7 +1,7 @@
 const { createProxyMiddleware } = require("http-proxy-middleware");
 module.exports = function (app) {
     app.use(createProxyMiddleware(`/api`, {
-        target: "http://localhost:8055",
+        target: "http://localhost:8085",
         changeOrigin: true,
         pathRewrite: {
             "^/api": ""
